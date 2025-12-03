06bd9ea9eb26aea725bea084d303dac355aefd9f
Jenifer29b <Jenifer29b@gmail.com>
2025-12-03 17:35:32 +0530

    task completed

---
+++ Day 6/task1.js
@@ -0,0 +1,12 @@
+var addTwoPromises = async function(promise1, promise2) {
+    let value1 = await promise1;
+    let value2 = await promise2;
+
+    return (value1 +value2)
+
+
+};
+addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+   .then(console.log); // 4
+
+
